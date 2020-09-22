d48a15387c9f96d0aea3ed00f3b8de1bcd9ffa81
MichaelHo <michelo851a1203@gmail.com>
2020-09-22 19:18:03 +0800

    deploy

---
+++ precache-manifest.2300a452c50f03740441790bbdb8cac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d009dfbe61b33e3caffff14c009686",
+    "url": "/testimageqrcode/img/01.27d009df.png"
+  },
+  {
+    "revision": "d2c07826d04e4fb35a17c0a82b02786c",
+    "url": "/testimageqrcode/index.html"
+  },
+  {
+    "revision": "1b7dd0d7ddeea8205854",
+    "url": "/testimageqrcode/js/app.719a9f2b.js"
+  },
+  {
+    "revision": "d10931433e687fc27f01",
+    "url": "/testimageqrcode/js/chunk-052c0185.1b55f2b2.js"
+  },
+  {
+    "revision": "929e775d0456a11eb882",
+    "url": "/testimageqrcode/js/chunk-vendors.defa23c3.js"
+  },
+  {
+    "revision": "d9c0082656c8a087e01a00601bb668c4",
+    "url": "/testimageqrcode/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testimageqrcode/robots.txt"
+  }
+]);
